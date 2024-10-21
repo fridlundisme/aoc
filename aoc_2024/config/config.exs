a45839c0f27b3866ec07ae138b87b7022a0de983
Joakim Fridlund <friddelund@gmail.com>
2024-10-21 23:33:21 +0200

    added requetst and caching for AOC input

--- aoc_2024/config/config.exs
+++ aoc_2024/config/config.exs
@@ -1,3 +1,6 @@
 import Config
 
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
+
+config :aoc_2024,
+  session_cookie: System.get_env("AOC_SESSION_COOKIE")
--- aoc_2024/lib/aoc2024/input.ex
+++ aoc_2024/lib/aoc2024/input.ex
@@ -1,4 +1,39 @@
 defmodule Aoc2024.Input do
+  @moduledoc """
+  Input documentation
+  """
+  @doc """
+  Get input for `day` of the year
+  """
+  def get!(year \\ 2024, day)
+
   def get!(year, day) do
+    case in_cache?(year, day) do
+      true ->
+        File.read!(tmp_path(year, day))
+
+      _ ->
+        download!(year, day)
+    end
+  end
+
+  defp in_cache?(year, day) do
+    File.exists?(tmp_path(year, day))
+  end
+
+  defp download!(year, day) do
+    cookie = Application.get_env(:aoc_2024, :session_cookie)
+
+    {:ok, resp} =
+      Req.get("https://adventofcode.com/#{year}/day/#{day}/input",
+        headers: %{cookie: "session=#{cookie}"}
+      )
+
+    resp.body |> then(&File.write!(tmp_path(year, day), &1))
+    resp.body
+  end
+
+  defp tmp_path(year, day) do
+    "/tmp/aoc_#{year}_#{day}.txt"
   end
 end
