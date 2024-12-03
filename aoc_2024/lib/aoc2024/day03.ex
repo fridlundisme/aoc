cfc61284b288196f6680611760b6ceb3fbcfbb85
Joakim Fridlund <friddelund@gmail.com>
2024-12-03 23:29:44 +0100

    refactor: extract common scanning of numbers to own function

--- aoc_2024/lib/aoc2024/day03.ex
+++ aoc_2024/lib/aoc2024/day03.ex
@@ -1,28 +1,27 @@
 defmodule Aoc2024.Day03 do
   def part1(input) do
-    re_multipliers = ~r/mul\(\d{1,3},\d{1,3}\)/
-
-    Regex.scan(re_multipliers, input)
-    |> List.flatten()
-    |> Enum.map(&create_numbers/1)
+    scan_for_numbers(input)
     |> Enum.map(fn x -> x |> Enum.reduce(&(&1 * &2)) end)
     |> Enum.sum()
   end
 
   def part2(input) do
-    re_multipliers = ~r/mul\(\d{1,3},\d{1,3}\)/
     re_ignore = ~r/don't\(\).+do\(\)|don't\(\).+$/sU
 
     Regex.split(re_ignore, input)
     |> Enum.map(fn x ->
-      Regex.scan(re_multipliers, x)
-      |> List.flatten()
-      |> Enum.map(&create_numbers/1)
+      scan_for_numbers(x)
       |> Enum.reduce(0, fn x, acc ->
         acc + Enum.reduce(x, &(&1 * &2))
       end)
     end)
     |> Enum.sum()
+  end
+
+  def scan_for_numbers(input) do
+    Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)/, input)
+    |> List.flatten()
+    |> Enum.map(&create_numbers/1)
   end
 
   def create_numbers(text) do
